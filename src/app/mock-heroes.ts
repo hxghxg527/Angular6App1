16384b1a761a10d6b6de4044d508d0297b67d6f3
Michael Hu <michael.hu@ef.com>
2018-08-29 11:38:11 +0800

    upload angular6 project basic code.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import {Hero} from './hero';
+
+export const HEROES:Hero[] = [
+  {
+    id: 11,
+    name: 'NiNi'
+  },
+  {
+    id: 12,
+    name: 'Fake'
+  },
+  {
+    id: 13,
+    name: 'Tbo'
+  },
+  {
+    id: 14,
+    name: 'Tata'
+  },
+  {
+    id: 15,
+    name: 'Rolin'
+  }
+];
